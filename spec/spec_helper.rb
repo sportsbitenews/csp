d884184fb8b3472192488ddf13dd7593e379f864
Girts Kesteris <girts.kesteris@gmail.com>
2014-06-12 13:33:57 +0300

    Spec optimised

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,6 @@ end
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}

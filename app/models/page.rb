141e29ca09ba37a35178dff909fb6772db5ce544
Sacristan <girts.kesteris@gmail.com>
2014-07-11 12:19:49 +0300

    Page fixed double accessor names

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
-  attr_accessible :name, :name, :template
+  attr_accessible :name, :template
   attr_accessible :page_function_group, :page_function_group_id
 
   has_one :page_function_group

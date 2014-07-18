4491db3c0d3cf0676bf02147a67292894f243c39
Sacristan <girts.kesteris@gmail.com>
2014-07-18 12:13:35 +0300

    Added SH to tests II

--- spec/models/pipeline_spec.rb
+++ spec/models/pipeline_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Pipeline, :type => :model do
           ctry_pipeline = FactoryGirl.create(:pipeline, locale: "es", country_id: Country.get_main.id, group: @group)
           pipeline = FactoryGirl.create(:pipeline, group: @group)
 
-          expect(Pipeline.locate domain: @group, country: Country.get_main, locale: "lv").to eq main_locale_ctry_pipeline
+          expect(Pipeline.locate domain: group, country: Country.get_main, locale: "lv").to eq main_locale_ctry_pipeline
         end
         it "should return country and locale that isnt main, if there is no country/locale main pipeline" do
 
@@ -38,7 +38,7 @@ RSpec.describe Pipeline, :type => :model do
           main_ctry_pipeline = FactoryGirl.create(:pipeline, locale: "es", country_id: Country.get_main.id, main: true, group: @group)
           ctry_pipeline = FactoryGirl.create(:pipeline, locale: "es", country_id: Country.get_main.id, group: group)
 
-          expect(Pipeline.locate domain: @group, country: Country.get_main, locale: "lv").to eq locale_ctry_pipeline
+          expect(Pipeline.locate domain: group, country: Country.get_main, locale: "lv").to eq locale_ctry_pipeline
         end
         it "should return by main country pipe" do
           
@@ -46,20 +46,20 @@ RSpec.describe Pipeline, :type => :model do
           main_ctry_pipeline = FactoryGirl.create(:pipeline, locale: "es", country_id: Country.get_main.id, main: true, group: @group)
           ctry_pipeline = FactoryGirl.create(:pipeline, locale: "es", country_id: Country.get_main.id, group: @group)
 
-          expect(Pipeline.locate domain: @group, country: Country.get_main, locale: "lv").to eq main_ctry_pipeline
+          expect(Pipeline.locate domain: group, country: Country.get_main, locale: "lv").to eq main_ctry_pipeline
         end
         it "should return pipeline by country only" do
           
           main_locale_ctry_pipeline_lt = FactoryGirl.create(:pipeline, locale: "lv", country_id: Country.find_by(code: "lt"), main: true, group: @group)
           ctry_pipeline = FactoryGirl.create(:pipeline, locale: "es", country_id: Country.get_main.id, group: @group)
 
-          expect(Pipeline.locate domain: @group, country: Country.get_main, locale: "lv").to eq ctry_pipeline
+          expect(Pipeline.locate domain: group, country: Country.get_main, locale: "lv").to eq ctry_pipeline
         end
 
         it "should fallback" do
           pipeline = FactoryGirl.create(:pipeline, group: @group)
 
-          expect(Pipeline.locate domain: @group, country: Country.get_main, locale: "lv").to eq pipeline
+          expect(Pipeline.locate domain: group, country: Country.get_main, locale: "lv").to eq pipeline
         end
       end
     end

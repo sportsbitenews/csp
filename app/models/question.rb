f05ecb61a701b868bdfb78a5b48765c2d6e37bcc
Sacristan <girts.kesteris@gmail.com>
2014-07-24 17:40:07 +0300

    Added Identifier to Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  attr_accessible :I18n_key
+  attr_accessible :I18n_key, :identifier
   attr_accessible :answers, :logs
 
   has_many :answers
--- db/migrate/20140723093052_create_questions.rb
+++ db/migrate/20140723093052_create_questions.rb
@@ -1,6 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.string :identifier
       t.string :I18n_key
       t.timestamps
     end

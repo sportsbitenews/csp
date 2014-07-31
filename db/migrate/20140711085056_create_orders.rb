d5da820b2a3b958649fdccd2e9a26d504ee2de85
Sacristan <girts.kesteris@gmail.com>
2014-07-31 11:21:15 +0300

    Default status order NEW

--- db/migrate/20140711085056_create_orders.rb
+++ db/migrate/20140711085056_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.string :email, index: true
-      t.string :status
+      t.string :status, default: "new"
       t.string :locale
       t.integer :category
       t.text :ga_data
